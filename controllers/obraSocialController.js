43e413664645baaaba0156bf66bca13349165497
brunoc88 <brunocerutti88@gmail.com>
2024-11-24 11:45:37 -0300

    Update obraSocialController.js

--- controllers/obraSocialController.js
+++ controllers/obraSocialController.js
@@ -137,6 +137,10 @@ exports.editarObraSocialPatch = async (req, res) => {
     try {
         const data = req.body; 
         const id = req.params.id;
+        const buscarObra = await ObraSocial.findOne({where:{nombre:data.nombre}});
+        if(buscarObra){
+            return res.status(400).json('Ya existe esa obra!');
+        }
         await ObraSocial.update({ nombre: data.nombre}, { where: { id } });
         res.redirect('/obra/index');
     } catch (error) {
